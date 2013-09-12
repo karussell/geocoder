8612c627bc075d25074cde1241a8b96f49ae1ea7
Peter <peathal@yahoo.de>
2013-09-12 18:47:34 +0200

    made first feeding working, plus tests

--- src/main/java/com/graphhopper/geocoder/JsonFeeder.java
+++ src/main/java/com/graphhopper/geocoder/JsonFeeder.java
@@ -1,7 +1,42 @@
 package com.graphhopper.geocoder;
 
 import com.github.jillesvangurp.osm2geojson.OsmPostProcessor;
+import com.github.jillesvangurp.osm2geojson.OsmPostProcessor.JsonWriter;
+import com.github.jillesvangurp.osm2geojson.OsmPostProcessor.OsmType;
+import com.github.jsonj.JsonArray;
+import com.github.jsonj.JsonElement;
+import com.github.jsonj.JsonObject;
 import com.github.jsonj.tools.JsonParser;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map.Entry;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.Requests;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.StopWatch;
+import org.elasticsearch.common.geo.builders.LineStringBuilder;
+import org.elasticsearch.common.geo.builders.PolygonBuilder;
+import org.elasticsearch.common.geo.builders.ShapeBuilder;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.common.xcontent.json.JsonXContent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Peter Karich
@@ -9,12 +44,230 @@ import com.github.jsonj.tools.JsonParser;
 public class JsonFeeder {
 
     public static void main(String[] args) {
-        new JsonFeeder().feed();
+        new JsonFeeder().start();
+    }
+
+    public static Client createClient(String cluster, String url, int port) {
+        Settings s = ImmutableSettings.settingsBuilder().put("cluster.name", cluster).build();
+        TransportClient tmp = new TransportClient(s);
+        tmp.addTransportAddress(new InetSocketTransportAddress(url, port));
+        return tmp;
+    }
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    private Client client;
+    private String poiType = "poi";
+    private String wayType = "way";
+    private String poiIndex = "poi";
+    private String wayIndex = "way";
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void start() {
+        // "failed to get node info for..." -> wrong elasticsearch version for client vs. server
+        // String cluster = "graphhopper";
+        String cluster = "elasticsearch";
+        String host = "localhost";
+        int port = 9300;
+        setClient(createClient(cluster, host, port));
+        feed();
     }
 
     public void feed() {
-        OsmPostProcessor processor = new OsmPostProcessor(new JsonParser());
+        final int bulkSize = 1000;
+        initIndices();
+
+        OsmPostProcessor processor = new OsmPostProcessor(new JsonParser()) {
+            StopWatch sw = new StopWatch().start();
+            long counter = 0;
+
+            @Override
+            protected JsonWriter createJsonWriter(OsmType type) throws IOException {
+                final String tmpType = type.toString().toLowerCase();
+                return new JsonWriter() {
+                    List<JsonObject> list = new ArrayList<JsonObject>(bulkSize);
+
+                    @Override
+                    public void add(JsonObject json) throws IOException {
+                        list.add(json);
+                        if (list.size() >= bulkSize) {
+                            counter += list.size();
+                            sw.stop();
+                            logger.info("took: " + (float) sw.totalTime().getSeconds() / counter);
+                            sw.start();
+                            bulkUpdate(list, tmpType, tmpType);
+                            list.clear();
+                        }
+                    }
+
+                    @Override
+                    public void close() throws IOException {
+                    }
+                };
+            }
+        };
         processor.processNodes();
         processor.processWays();
+    }
+
+    public Collection<Integer> bulkUpdate(Collection<JsonObject> objects, String indexName, String indexType) {
+        // now using bulk API instead of feeding each doc separate with feedDoc
+        BulkRequestBuilder brb = client.prepareBulk();
+        for (JsonObject o : objects) {
+            String id = o.getString("id");
+            if (id == null) {
+                logger.warn("Skipped object without id when bulkUpdate:" + o);
+                continue;
+            }
+
+            try {
+                XContentBuilder source = createDoc(o);
+                IndexRequest indexReq = Requests.indexRequest(indexName).type(indexType).id(id).source(source);
+                brb.add(indexReq);
+            } catch (Exception ex) {
+                logger.warn("cannot add object " + id + " -> " + o.toString(), ex);
+            }
+        }
+        if (brb.numberOfActions() > 0) {
+            BulkResponse rsp = brb.execute().actionGet();
+            if (rsp.hasFailures()) {
+                List<Integer> list = new ArrayList<Integer>(rsp.getItems().length);
+                for (BulkItemResponse br : rsp.getItems()) {
+                    if (br.isFailed()) {
+                        logger.warn("Cannot index object " + br.getId() + ". Error:" + br.getFailureMessage());
+                        list.add(br.getItemId());
+                    }
+                }
+                return list;
+            }
+        }
+
+        return Collections.emptyList();
+    }
+
+    // {"id":"osmnode/1411809098","title":"Bensons Rift",
+    //      "geometry":{"type":"Point","coordinates":[-75.9839922,44.3561003]},
+    //      "categories":{"osm":["natural:water"]}}
+    public XContentBuilder createDoc(JsonObject o) throws IOException {
+        XContentBuilder b = JsonXContent.contentBuilder().startObject();
+
+        for (Entry<String, JsonElement> e : o.entrySet()) {
+            JsonElement el = e.getValue();
+            String key = e.getKey();
+            if (key.equalsIgnoreCase("id")) {
+                // handled separately -> no need to feed for now
+                continue;
+            } else if (key.equalsIgnoreCase("geometry")) {
+                JsonObject jsonObj = el.asObject();
+                String type = jsonObj.getString("type");
+
+                if ("Point".equalsIgnoreCase(type)) {
+                    JsonArray arr = jsonObj.getArray("coordinates");
+                    // keep lon,lat
+                    b.field("location", new Object[]{arr.get(0), arr.get(1)});
+
+                } else if ("LineString".equalsIgnoreCase(type)) {
+                    JsonArray arr = jsonObj.getArray("coordinates");
+                    List<Object[]> mainList = new ArrayList<Object[]>();
+                    for (JsonArray innerArr : arr.arrays()) {
+                        // keep lon,lat
+                        mainList.add(new Object[]{innerArr.get(0), innerArr.get(1)});
+                    }
+                    b.field("shape", mainList);
+
+                } else if ("Polygon".equalsIgnoreCase(type)) {
+                    // polygon is an array of array of array
+                    // "geometry":{"type":"Polygon","coordinates":[[[..]]]
+                    List<List<Object[]>> mainList = new ArrayList<List<Object[]>>();
+                    JsonArray arr = jsonObj.getArray("coordinates");
+                    for (JsonArray innerArr : arr.arrays()) {
+                        List<Object[]> tmpList = new ArrayList<Object[]>();
+                        mainList.add(tmpList);
+                        for (JsonArray innerstArr : innerArr.arrays()) {
+                            tmpList.add(new Object[]{innerstArr.get(0), innerstArr.get(1)});
+                        }
+                    }
+                    b.field("shape", mainList);
+                } else {
+                    throw new IllegalStateException("wrong geometry format:" + key + " -> " + el.toString());
+                }
+            } else if (key.equalsIgnoreCase("categories")) {
+                b.field("categories", el.asObject().getArray("osm"));
+            } else if (key.equalsIgnoreCase("title")) {
+                b.field("title", el.asString());
+            } else if (key.equalsIgnoreCase("address")) {
+                // object ala {"housenumber":"555","street":"5th Avenue"}
+                b.field("address", el);
+            } else if (key.equalsIgnoreCase("links")) {
+                // array ala  [{"href":"http://www.fairwaymarket.com/"}]
+                b.field("links", el);
+            } else {
+                b.field(key, el);
+                logger.warn("Not explicitely supported " + el.type() + ": " + key + " -> " + el.toString());
+            }
+        }
+
+        b.endObject();
+        return b;
+    }
+
+    public void initIndices() {
+        initIndex(poiIndex, poiType);
+        initIndex(wayIndex, wayType);
+    }
+
+    private void createIndex(String indexName) {
+        boolean log = true;
+        try {
+            client.admin().indices().create(new CreateIndexRequest(indexName)).actionGet();
+            if (log)
+                logger.info("Created index: " + indexName);
+        } catch (Exception ex) {
+            if (log)
+                logger.info("Index " + indexName + " already exists");
+        }
+    }
+
+    private void createIndexAndPutMapping(String indexName, String type) {
+        boolean log = true;
+        try {
+            InputStream is = getClass().getResourceAsStream(type + ".json");
+            String mappingSource = toString(is);
+            client.admin().indices().create(new CreateIndexRequest(indexName).mapping(type, mappingSource)).actionGet();
+            if (log)
+                logger.info("Created index: " + indexName);
+        } catch (Exception ex) {
+            if (log)
+                logger.info("Index " + indexName + " already exists");
+        }
+    }
+
+    private void initIndex(String indexName, String type) {
+        createIndexAndPutMapping(indexName, type);
+//        createIndex(indexName);
+//
+//        InputStream is = getClass().getResourceAsStream(type + ".json");
+//        try {
+//            String mappingSource = toString(is);
+//            client.admin().indices().putMapping(new PutMappingRequest(indexName).type(type).source(mappingSource)).actionGet();
+//        } catch (Exception ex) {
+//            throw new RuntimeException(ex);
+//        }
+    }
+
+    public static String toString(InputStream is) throws IOException {
+        if (is == null)
+            throw new IllegalArgumentException("stream is null!");
+
+        BufferedReader bufReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = bufReader.readLine()) != null) {
+            sb.append(line);
+            sb.append('\n');
+        }
+        bufReader.close();
+        return sb.toString();
     }
 }
---
+++ src/test/java/com/graphhopper/geocoder/JsonFeederTest.java
@@ -0,0 +1,111 @@
+package com.graphhopper.geocoder;
+
+import com.github.jsonj.JsonArray;
+import com.github.jsonj.JsonObject;
+import static com.github.jsonj.tools.JsonBuilder.$;
+import static com.github.jsonj.tools.JsonBuilder._;
+import static com.github.jsonj.tools.JsonBuilder.array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.elasticsearch.action.admin.indices.refresh.RefreshRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.common.geo.builders.ShapeBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryStringQueryBuilder;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.elasticsearch.index.query.QueryBuilders;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author Peter Karich
+ */
+public class JsonFeederTest extends AbstractNodesTests {
+
+    protected Client client;
+    private String wayIndex = "way";
+    private String wayType = "way";
+    private String poiIndex = "poi";
+    private String poiType = "poi";
+
+    @BeforeClass public void createNodes() throws Exception {
+        startNode("node1");
+        client = client("node1");
+    }
+
+    @AfterClass public void closeNodes() {
+        client.close();
+        closeAllNodes();
+    }
+
+    @Test
+    public void testFeedPoint() {
+        JsonFeeder feeder = new JsonFeeder();
+        feeder.setClient(client);
+        feeder.initIndices();
+
+        List<JsonObject> list = new ArrayList<JsonObject>();
+        JsonArray coordinates = array(-11, 11);
+        JsonObject geo = $(_("type", "Point"), _("coordinates", coordinates));
+        list.add($(_("id", "osmnode/123"), _("geometry", geo), _("title", "testing it today")));
+
+        Collection<Integer> res = feeder.bulkUpdate(list, poiIndex, poiType);
+        assertThat(res.size(), equalTo(0));
+        refresh(poiIndex);
+        assertThat(client.prepareCount(poiIndex).execute().actionGet().getCount(), equalTo(1L));
+
+        QueryBuilder builder = QueryBuilders.queryString("today").
+                defaultField("title").
+                defaultOperator(QueryStringQueryBuilder.Operator.AND);
+        SearchResponse rsp = client.prepareSearch(poiIndex).setTypes(poiType).setQuery(builder).execute().actionGet();
+        assertThat(rsp.getHits().getTotalHits(), equalTo(1L));
+    }
+    
+//    {"id":"osmway/100198671","title":"Depaula Chevrolet Hummer",
+//       "geometry":{"type":"Polygon","coordinates":[[[-73.7882444,42.6792747],[-73.7880386,42.6790894],[-73.7880643,42.6790714],[-73.7879537,42.6789736],[-73.7877787,42.6789659],[-73.7876115,42.6790637],[-73.7876423,42.6790997],[-73.787156,42.6794008],[-73.7870608,42.6793184],[-73.7866569,42.6795603],[-73.7869734,42.6798459],[-73.78703,42.6798124],[-73.7871689,42.6799385],[-73.7882444,42.6792747]]]},
+//       "categories":{"osm":["building:yes","shop:car","building"]},
+//       "address":{"city":"Albany","country":"US","housenumber":"785","postcode":"12206","street":"Central Avenue"}
+//    }
+    @Test
+    public void testFeedLineString() {
+        JsonFeeder feeder = new JsonFeeder();
+        feeder.setClient(client);
+        feeder.initIndices();
+
+        List<JsonObject> list = new ArrayList<JsonObject>();
+        JsonArray coordinates = array();
+        coordinates.add(array(11, 11));
+        coordinates.add(array(22, 22));
+        coordinates.add(array(33, 33));
+        JsonObject geo = $(_("type", "LineString"), _("coordinates", coordinates));
+        list.add($(_("id", "osmway/123"), _("geometry", geo), _("title", "testing it today")));
+
+        Collection<Integer> res = feeder.bulkUpdate(list, wayIndex, wayType);
+        assertThat(res.size(), equalTo(0));
+        refresh(wayIndex);
+        assertThat(client.prepareCount(wayIndex).execute().actionGet().getCount(), equalTo(1L));
+
+        QueryBuilder builder = QueryBuilders.queryString("today").
+                defaultField("title").
+                defaultOperator(QueryStringQueryBuilder.Operator.AND);
+        SearchResponse rsp = client.prepareSearch(wayIndex).setTypes(wayType).setQuery(builder).execute().actionGet();
+        assertThat(rsp.getHits().getTotalHits(), equalTo(1L));
+
+        ShapeBuilder query = ShapeBuilder.newEnvelope().topLeft(-122.88, 48.62).bottomRight(-122.82, 48.54);
+
+        // TODO
+//        rsp = client.prepareSearch().setQuery(filteredQuery(matchAllQuery(),
+//                geoIntersectionFilter("location", query)))
+//                .execute().actionGet();
+//        assertThat(rsp.getHits().getTotalHits(), equalTo(1L));
+    }
+
+    protected void refresh(String indexName) {
+        client.admin().indices().refresh(new RefreshRequest(indexName)).actionGet();
+    }
+}
