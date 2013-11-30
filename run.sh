a11fead986f04d8a94223a896c17be489f6f4f7a
Peter <peathal@yahoo.de>
2013-11-30 14:18:20 +0100

    included server into run.sh

--- run.sh
+++ run.sh
@@ -7,25 +7,30 @@ if [ "x$MAVEN_HOME" = "x" ]; then
  MAVEN_HOME=`mvn -v | grep "Maven home" | cut -d' ' -f3`
 fi
 
-
+ACTION=$1
 JAVA_OPTS="-XX:PermSize=60m -XX:MaxPermSize=60m -Xmx200m -Xms200m -server"
 VERSION=`grep  "<name>" -B 1 pom.xml | grep version | cut -d'>' -f2 | cut -d'<' -f1`
 JAR=target/geocoder-$VERSION-jar-with-dependencies.jar
 
- if [ ! -f "$JAR" ]; then
-    echo "## now building jar: $JAR"
-    echo "## using maven at $MAVEN_HOME"
-    #mvn clean
-    "$MAVEN_HOME/bin/mvn" -DskipTests=true install assembly:single > /tmp/geocoder-compile.log
-    returncode=$?
-    if [[ $returncode != 0 ]] ; then
-        echo "## compilation of core failed"
-        cat /tmp/geocoder-compile.log
-        exit $returncode
-    fi      
-  else
-    echo "## existing jar found $JAR"
-  fi
+if [ ! -f "$JAR" ]; then
+  echo "## now building jar: $JAR"
+  echo "## using maven at $MAVEN_HOME"
+  #mvn clean
+  "$MAVEN_HOME/bin/mvn" -DskipTests=true install assembly:single > /tmp/geocoder-compile.log
+  returncode=$?
+  if [[ $returncode != 0 ]] ; then
+      echo "## compilation of core failed"
+      cat /tmp/geocoder-compile.log
+      exit $returncode
+  fi      
+else
+  echo "## existing jar found $JAR"
+fi
 
-
-"$JAVA" $JAVA_OPTS -cp "$JAR" com.graphhopper.geocoder.JsonFeeder
+if [ "x$ACTION" = "xfeed" ]; then
+  "$JAVA" $JAVA_OPTS -cp "$JAR" com.graphhopper.geocoder.JsonFeeder
+elif [ "x$ACTION" = "xserver" ]; then
+  "$JAVA" $JAVA_OPTS -cp "$JAR" com.graphhopper.geocoder.http.HttpServerMain
+else
+  echo unknown action $ACTION
+fi
